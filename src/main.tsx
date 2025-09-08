67a14017335e6adf9574ba36d7a899c063d032d8
project-zulu-ai <phillybigsmiles@gmail.com>
2025-09-09 07:41:40 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import Index from "./pages/Index.tsx";
+import { AppGenerator } from "@/components/AppGenerator";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Default app wrapper */}
+        <Route path="/*" element={<App />}>
+          {/* Landing page */}
+          <Route index element={<Index />} />
+          {/* Generator route */}
+          <Route path="generator" element={<AppGenerator onBack={() => (window.location.href = "/")} />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
